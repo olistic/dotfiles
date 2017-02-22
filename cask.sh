2ea25122e86fea52537051193d1b13494e3fce7c
Matias Olivera <moliverafreire@gmail.com>
2017-02-22 08:53:39 -0300

    Fix cask.sh

--- cask.sh
+++ cask.sh
@@ -2,11 +2,12 @@
 
 # Install native apps using Cask.
 
+# Get Cask.
+brew tap caskroom/cask
+brew tap caskroom/versions
+
 # Make sure we’re using the latest Homebrew.
 brew update
-
-# Upgrade any already-installed formulae.
-brew upgrade
 
 # Install apps.
 brew cask install atom
@@ -14,5 +15,7 @@ brew cask install google-chrome
 brew cask install google-chrome-canary
 brew cask install hyper
 brew cask install java
+brew cask install paw
 brew cask install slack
+brew cask install spotify
 brew cask install xquartz
