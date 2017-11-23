6e5cc6a8358a362f9132c3f7a39b5b6f20f8d1d2
Matias Olivera <moliverafreire@gmail.com>
2017-11-22 21:08:50 -0300

    Remove pyenv

--- .zshrc
+++ .zshrc
@@ -98,12 +98,3 @@ export PIP_REQUIRE_VIRTUALENV=true
 gpip() {
   PIP_REQUIRE_VIRTUALENV="" pip "$@"
 }
-
-# pyenv.
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-
-pyenv() {
-  eval "$(command pyenv init -)"
-  pyenv "$@"
-}
--- install.sh
+++ install.sh
@@ -44,12 +44,6 @@ fi
 # Install virtualenv.
 pip install virtualenv
 
-# Install pyenv and pyenv-virtualenv if not installed.
-if [ ! -d ~/.pyenv ]; then
-  git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-  git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-fi
-
 ###############################################################################
 # Shell                                                                       #
 ###############################################################################
