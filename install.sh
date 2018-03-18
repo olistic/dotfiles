bf5439fe8e62b09acc2ead32ce5c9e959e52c3a4
Matias Olivera <moliverafreire@gmail.com>
2018-03-18 12:51:11 -0300

    Improve installation script
     - Add upgrade logic if tools are already installed
    
     - Make virtualenv installation idempotent

--- install.sh
+++ install.sh
@@ -31,18 +31,30 @@ brew cleanup
 # Node.js                                                                     #
 ###############################################################################
 
-# Install nvm if not installed.
-if [ ! -d ~/.nvm ]; then
-  git clone https://github.com/creationix/nvm.git ~/.nvm
-  (cd ~/.nvm && git checkout v0.33.6)
+# Install nvm if not installed or upgrade it.
+NVM_DIR="$HOME/.nvm"
+if [ ! -d "$NVM_DIR" ]; then
+  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+  (
+    cd "$NVM_DIR"
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+  )
+else
+  (
+    cd "$NVM_DIR"
+    git fetch origin
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+  )
 fi
 
 ###############################################################################
 # Python                                                                      #
 ###############################################################################
 
-# Install virtualenv.
-pip install virtualenv
+# Install virtualenv if not installed.
+if ! which virtualenv > /dev/null; then
+  pip install virtualenv
+fi
 
 ###############################################################################
 # Shell                                                                       #
@@ -52,18 +64,32 @@ pip install virtualenv
 if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
   echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells;
   chsh -s /usr/local/bin/zsh;
-fi;
+fi
 
 # Install Oh My Zsh if not installed.
-if [ ! -d ~/.oh-my-zsh ]; then
+OH_MY_ZSH_DIR="$HOME/.oh-my-zsh"
+if [ ! -d "$OH_MY_ZSH_DIR" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
-fi;
+else
+  env ZSH="$ZSH" sh "$ZSH/tools/upgrade.sh"
+fi
 
 # Install pure prompt if not installed.
-if [ ! -d ~/.pure ]; then
-  git clone https://github.com/sindresorhus/pure.git ~/.pure
-  ln -s ~/.pure/pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup
-  ln -s ~/.pure/async.zsh /usr/local/share/zsh/site-functions/async
+PURE_DIR="$HOME/.pure"
+if [ ! -d "$PURE_DIR" ]; then
+  git clone https://github.com/sindresorhus/pure.git "$PURE_DIR"
+  (
+    cd "$PURE_DIR"
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+  )
+  ln -s "$PURE_DIR/pure.zsh" /usr/local/share/zsh/site-functions/prompt_pure_setup
+  ln -s "$PURE_DIR/async.zsh" /usr/local/share/zsh/site-functions/async
+else
+  (
+    cd "$PURE_DIR"
+    git fetch origin
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+  )
 fi
 
 # Install dotfiles.
